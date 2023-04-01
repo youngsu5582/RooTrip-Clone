b40e1f982d371e6cd7bf75b76f1958b2d94391bb
youngsu5582 <yuyoungsu5582@gmail.com>
2023-04-01 13:58:23 +0900

    [Feat] Swagger Module 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import InitSwagger from './loaders/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  await InitSwagger(app);
   await app.listen(8000);
   console.log('Server Open!');
 }
