3aaabaab637c3f132828a4d735a9a4fd56beed4a
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2023-04-01 14:14:28 +0900

    [Feat] env Setting File 추가 & Server 구동시 try catch 문 추가 (#2)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import InitSwagger from './loaders/swagger';
+import env from './loaders/env';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   await InitSwagger(app);
-  await app.listen(8000);
-  console.log('Server Open!');
+  await app.listen(env.app.port)
+  .then(()=>console.log('Server Open!'))
+  .catch((err)=>console.error(err));
+  
 }
 bootstrap();
