1c720ffa02df12a4da542e7854d517e778b706f0
youngsu5582 <yuyoungsu5582@gmail.com>
2023-04-16 15:38:04 +0900

    [Refactoring] User Service 에러 해결 및 리팩토링

--- src/test/units/user.service.spec.ts
+++ src/test/units/user.service.spec.ts
@@ -1,68 +1,92 @@
-import { ConfigModule } from '@nestjs/config';
-import { Test, TestingModule } from '@nestjs/testing';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/models/tables/user.entity';;
-import { TypeOrmMoudleOptions } from 'src/database/typeorm';
-import { UserService } from 'src/providers/user.service';
-import { UserModule } from 'src/module/user.module';
-import { CreateUserDto } from 'src/models/dtos/create-user-dto';
-import { typeormConfig } from 'src/config';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppModule } from "src/app.module";
+import { CreateUserDto } from "src/models/dtos/create-user-dto";
+import { UsersRepository } from "src/models/repositories/user.repository";
+import { UserService } from "src/providers/user.service";
 
-describe('User Service',()=>{
-  let service : UserService;
-  let module :TestingModule;
-  beforeAll(async()=>{
+const userDto: CreateUserDto = {
+  email: "test@gmail.com",
+  gender: "m",
+  name: "test",
+  nickname: "testnickname",
+  password: "testest1234!",
+  phoneNumber: "01012345678"
+};
+describe("User service", () => {
+  let service: UserService;
+  let repository: UsersRepository;
+  let module: TestingModule;
+  beforeAll(async () => {
     module = await Test.createTestingModule({
-      imports:[
-        TypeOrmModule.forRootAsync(TypeOrmMoudleOptions),
-        TypeOrmModule.forFeature([User]),
-        ConfigModule.forRoot({
-          load:[typeormConfig],
-          isGlobal:true}),
-        UserModule,
-      ],
-      controllers:[],
-      providers:[],
+      imports: [AppModule],
+      providers: [
+        UserService,
+        {
+          provide: UsersRepository,
+          useValue: {
+            UsersRepository
+          }
+        }
+      ]
     }).compile();
     service = module.get<UserService>(UserService);
-  })
-  
-  describe('0. Setting',()=>{
-    it('Controller , Service ',async ()=>{
-      expect(service).toBeDefined();
-    })
+    await service.create(userDto);
+    repository = module.get<UsersRepository>(UsersRepository);
   });
-  describe('1. Create', ()=>{
-    it('1-1 Create Complete',async()=>{
-    const userDto : CreateUserDto={
-      email:'test@gmail.com',
-      gender:'m',
-      name:'test',
-      nickname:'testnickname',
-      password:'testest1234!',
-      phoneNumber:'01055825894'
-    }
-    const result = await service.create(userDto);
-    expect(result).toEqual({
-      status: true,
-      message: "회원가입 성공"
-    })
-  })
-  it('1-2 Duplicate Email should be return error',async ()=>{
-    const DuplicatedEmialDto : CreateUserDto={
-      email:'test1234@gmail.com',
-      gender:'m',
-      name:'test',
-      nickname:'testnickname',
-      password:'testest1234!',
-      phoneNumber:'01012345678'
-    };
-    await service.create(DuplicatedEmialDto);
-    DuplicatedEmialDto.phoneNumber='01056781234';
-    const result = await service.create(DuplicatedEmialDto);
-    expect(result).toEqual({
-      status: true,
-      message: "중복된 이메일이 있습니다."
-    })
-  })
-})})
+
+  afterAll(() => {
+    module.close();
+  });
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+  describe("create", () => {
+    afterAll(() => repository.delete({}));
+    it("1. Create", async () => {
+      const newUserDto: CreateUserDto = {
+        email: "test12@gmail.com",
+        gender: "m",
+        name: "testname",
+        nickname: "testnickname",
+        password: "testest1234!",
+        phoneNumber: "01011112222"
+      };
+      const result = await service.create(newUserDto);
+      expect(result).toEqual({
+        status: true,
+        message: "회원가입 성공"
+      });
+    });
+
+    it("2.If duplicated email , should return error ", async () => {
+      const duplicatedEmailDto: CreateUserDto = {
+        email: "test@gmail.com",
+        gender: "m",
+        name: "duplicated",
+        nickname: "duplicated",
+        password: "duplicated",
+        phoneNumber: "01022221111"
+      };
+      const result = await service.create(duplicatedEmailDto);
+      expect(result).toEqual({
+        status: false,
+        message: "중복된 이메일이 있습니다."
+      });
+    });
+    it("3. If duplicated phonenumber , should return error", async () => {
+      const duplicatedPhoneDto: CreateUserDto = {
+        email: "test1234@gmail.com",
+        gender: "m",
+        name: "duplicated",
+        nickname: "duplicated",
+        password: "duplicated",
+        phoneNumber: "01012345678"
+      };
+      const result = await service.create(duplicatedPhoneDto);
+      expect(result).toEqual({
+        status: false,
+        message: "중복된 전화번호가 있습니다."
+      });
+    });
+  });
+});
